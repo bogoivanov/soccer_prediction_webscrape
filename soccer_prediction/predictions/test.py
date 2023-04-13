e855c137e5dac347a9461f77ac958a523232fb43
bogoivanov <102627890+bogoivanov@users.noreply.github.com>
2023-04-13 12:11:50 +0300

    leagues added

--- soccer_prediction/predictions/test.py
+++ soccer_prediction/predictions/test.py
@@ -1,3 +1,7 @@
-x = 10
-result = "Even" if x % 2 == 0 else "Odd"
-print(result)
+# orders = ["Ivan: Pesho: Gosho"]
+# (name1, name2, name3) = orders[0].split(": ")
+# print(type((name1, name2, name3)))
+# print((name1, name2, name3))
+
+time_elapsed_for = 2 - 1
+print(f"Time elapsed : {time_elapsed_for} seconds")
